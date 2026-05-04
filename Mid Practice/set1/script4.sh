09cb85ee4e2a4d23a5b28a227e503b87802caf85
atik203 <atikurrahaman0305@gmail.com>
2026-05-04 21:27:49 +0600

    Add initial script4.sh for user input and greeting functionality

---
+++ Mid Practice/set1/script4.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# The read command in bash scripting is used to take input from the user or from another source and store it in one or more variables.
+
+# It is commonly used for interactive scripts, where the script pauses and waits for the user to type something.
+
+echo "Enter your name:"
+read name
+echo "Hello, $name"
